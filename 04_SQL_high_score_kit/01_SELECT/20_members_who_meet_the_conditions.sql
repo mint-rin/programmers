17296468ff79c932e441aeb7764ae57ad050bbcc
Park Harin <132567022+mint-rin@users.noreply.github.com>
2023-09-30 18:44:55 +0900

    Create 20_members_who_meet_the_conditions.sql

---
+++ 04_SQL_high_score_kit/01_SELECT/20_members_who_meet_the_conditions.sql
@@ -0,0 +1,6 @@
+-- USER_INFO 테이블에서 2021년에 가입한 회원 중 나이가 20세 이상 29세 이하인 회원이 몇 명인지 출력하는 SQL문을 작성해주세요.
+
+SELECT COUNT(*)
+FROM USER_INFO
+WHERE DATE_FORMAT(JOINED, '%Y') = 2021
+      AND AGE BETWEEN 20 AND 29
